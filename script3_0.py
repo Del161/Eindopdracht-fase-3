5ff201f0f574be88b6a7c5e820bf89a6b805fc12
dvegter <d.vegter@st.hanze.nl>
2022-11-01 15:35:09 +0100

    added data and updated python

--- script3_0.py
+++ script3_0.py
@@ -1,2 +1,52 @@
 import sys
 
+"""
+function_placeholder
+Author: Delshad Vegter 
+date: 01/11/2022
+version: 1
+"""
+def extract_microarray_content(input_name):
+    """
+    Opening of the given file.
+    :param input_name: the input name given in commandline
+    :return: list with content of the file
+    """
+
+    with open(input_name, "r") as file:
+        # set file lines to variable
+        file_content = list(file.readlines())
+
+    for lines in file_content:
+            lines = lines.split(",")
+            probe_ID = lines.pop(0)
+            probe_content = "".join(lines)
+            print(probe_ID, "test", probe_content)
+
+
+    return file_content
+
+
+
+def main():
+    """
+    Main, where all functions are ran in order
+    :return: /
+    """
+
+    arguments = sys.argv
+
+    if len(arguments) < 2:
+        raise Exception(
+            "No input filename given, please use commandline arguments. "
+            "(python3 script2_3.py file inputname, output filename )")
+    else:
+        # get the argument and set the variable
+        input_name = arguments[1]
+
+    array_file_content = extract_microarray_content(input_name)
+
+
+# to protect against problems when imported
+if __name__ == "__main__":
+    main()
