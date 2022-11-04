79718542f0e714318b15ea61141047baa231add1
dvegter <d.vegter@st.hanze.nl>
2022-11-04 10:58:08 +0100

    now only selects probes with highest gene values

--- script3_0.py
+++ script3_0.py
@@ -6,55 +6,110 @@ Author: Delshad Vegter
 date: 01/11/2022
 version: 1.1
 """
-def extract_microarray_content():
+def extract_microarray_content(gene_probes_dictionary):
     """
     Opening of the given file, extract microarray data to dictionary.
     :return: list with content of the file
     """
 
     # predefined variables
-    probe_ID = ""
-    probe_ID_list = []
+    average = 0
     microarray_data = {}
     index = 0
+    best_probes_list = []
+
+
 
     with open("./data/MicroarrayExpression.csv", "r") as file:
         # set file lines to variable
         file_content = list(file.readlines())
 
     # take each line, turn the id into a key and the content into the key content.
-    for lines in file_content:
-        lines = lines.strip().split(",")
-        probe_ID = lines.pop(0)
-        probe_content = "".join(lines)
-        probe_ID_list.append(probe_ID)
-        microarray_data[probe_ID] = probe_content
-
-        # find which line contains the identifier
-        if identifier in lines:
-            locations.append(index)
+    for line in file_content:
+        line = line.strip().split(",")
+        probe_id = line.pop(0)
+        probe_content = line[1:]
+        microarray_data[probe_id] = probe_content
         index += 1
 
-    return microarray_data, probe_ID_list
+    for genes in gene_probes_dictionary:
+        probe_numbers = gene_probes_dictionary[genes]
+        for probes in probe_numbers:
+            average = 0
+            rowvalues = []
 
-def extract_sample_annot():
+            # split the row at the commas to get the seperate values
+            microarray_row = microarray_data[probes]
+
+            # combine all the values in a probe
+            for values in microarray_row:
+                average += float(values)
+
+            # get the index of the highest value probe
+            average = average / len(microarray_row)
+            rowvalues.append(average)
+            highest_value = max(rowvalues)
+            best_probe_index = rowvalues.index(highest_value)
+        best_probes_list.append(probe_numbers[best_probe_index])
+
+        print(genes, best_probes_list)
+        best_probes_list = []
+
+
+
+
+
+    return
+
+def extract_sample_annot(identifier):
 
 
     with open("./data/SampleAnnot.csv", "r") as file:
         # set file lines to variable
         file_content = list(file.readlines())
 
-    probe_ID = ""
+    index = 0
     probes_data = {}
+    locations = []
 
     # take each line, turn the id into a key and the content into the key content.
-    for lines in file_content:
+    for lines in file_content[1:]:
         lines = lines.strip().split(",")
         probe_ID = lines.pop(0)
         probe_content = "".join(lines)
         probes_data[probe_ID] = probe_content
 
+    for lines in file_content:
+        # find which line contains the identifier
+        if identifier[0] in lines:
+            locations.append(index)
+        index += 1
+
     return probes_data
+
+def extract_genes():
+
+    # predefined vars
+    gene_id = ""
+    probe_id = ""
+    gene_probeid_list = ""
+    genes_list =[]
+    gene_probes_dictionary = {}
+
+    with open("./data/Probes.csv", "r") as file:
+        # set file lines to variable
+        file_content = list(file.readlines())
+    for lines in file_content[1:]:
+        lines = lines.split(",")
+        gene_id = lines.pop(2)
+        probe_id = lines.pop(0)
+
+        if gene_id in gene_probes_dictionary:
+            gene_probes_dictionary[gene_id].append(probe_id)
+        else:
+            gene_probes_dictionary[gene_id] = list([probe_id])
+
+    return gene_probes_dictionary
 
 
 def main():
@@ -65,16 +120,17 @@ def main():
 
     arguments = sys.argv
 
-# if len(arguments) < 2:
-#     raise Exception(
-#         "No input filename given, please use commandline arguments. "
-#         "(python3 script2_3.py file inputname, output filename )")
-# else:
-#     # get the argument and set the variable
-#     input_name = arguments[1]
+    if len(arguments) < 3:
+        raise Exception(
+            "No input filename given, please use commandline arguments. "
+            "(python3 script2_3.py identifier identifier2)")
+    else:
+        # get the argument and set the variable
+        identifier = [arguments[1], arguments[2]]
 
-    array_file_content, probe_ID_list = extract_microarray_content()
-    probes_data = extract_sample_annot()
+    probes_data = extract_sample_annot(identifier)
+    gene_probes_dictionary = extract_genes()
+    extract_microarray_content(gene_probes_dictionary)
 
 
 # to protect against problems when imported
